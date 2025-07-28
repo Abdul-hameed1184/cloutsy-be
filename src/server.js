4fe96eb368cd118f74101a10872e658628a24077
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-07-28 22:03:30 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import authRoutes from "../src/routes/auth.routes.js"
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+dotenv.config();
+
+app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://worldnumhub.vercel.app",
+];
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error("Blocked by CORS:", origin); // debug log
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, ()=> {
+  console.log(`server running on port ${PORT} `)
+  connectDB();
+})
