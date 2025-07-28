ff832a3bc3e72486265c8d667bb43856e5699f58
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-07-28 22:41:49 +0100

    cors origin update

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://worldnumhub.vercel.app",
+  "https://cloutsy.vercel.app/",
 ];
 app.use(cookieParser());
 app.use(
